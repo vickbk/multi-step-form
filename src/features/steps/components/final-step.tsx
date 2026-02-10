9f13e4c1f7556a89566d8a5c5bff7a067830d57d
vickbk <vickbake8@gmail.com>
2026-02-10 18:24:36 +0200

    feat: implement FinalStep component with thank you message and subscription confirmation

--- src/features/steps/components/final-step.tsx
+++ src/features/steps/components/final-step.tsx
@@ -1,3 +1,20 @@
+import { default as thanksImage } from "@/assets/images/icon-thank-you.svg";
+import { SROnly } from "@/shared/components/SROnly";
+import { Heading } from "@/shared/heading-manager/components/heading-managers";
+
 export const FinalStep = () => {
-  return <div>FinalStep</div>;
+  return (
+    <article>
+      <img src={thanksImage} alt="" />
+      <Heading>
+        Thank you <SROnly>for your subscription</SROnly>
+      </Heading>
+      <p>
+        Thanks for confirming your subscription! We hope you have fun using our
+        platform. If you ever need support, please feel free to email us at
+        support@loremgaming.com.
+      </p>
+      <button type="button">Subscribe another account</button>
+    </article>
+  );
 };
