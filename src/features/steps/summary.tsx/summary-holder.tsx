9fc38b39ab59e4cab3aef9380f8e518e7e801d1b
vickbk <vickbake8@gmail.com>
2026-02-12 14:02:51 +0200

    feat: update SummaryHolder styling to improve layout for non-first elements

--- src/features/steps/summary.tsx/summary-holder.tsx
+++ src/features/steps/summary.tsx/summary-holder.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const SummaryHolder = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="background p-4 first:rounded-t-2xl last:rounded-b-2xl c-grey-500 last:[&>div.mt-4]:hidden">
+    <div className="background p-4 not-first:pt-0 first:rounded-t-2xl last:rounded-b-2xl c-grey-500 last:[&>div.mt-4]:hidden">
       {children}
       <button type="button" className="underline">
         Change
