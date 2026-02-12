69e7f3cb7619416cd3eb11a148b7577601e7ee67
vickbk <vickbake8@gmail.com>
2026-02-12 14:31:49 +0200

    feat: enhance PlanSummary and SummaryHolder components with custom styling and layout improvements

--- src/features/steps/summary.tsx/plan-summary.tsx
+++ src/features/steps/summary.tsx/plan-summary.tsx
@@ -1,11 +1,19 @@
+import { SROnly } from "@/shared/components/SROnly";
 import { SummaryHolder } from "./summary-holder";
 
 export const PlanSummary = () => {
   return (
-    <SummaryHolder>
-      <dl>
-        <dt>plan</dt>
-        <dd>value</dd>
+    <SummaryHolder custom>
+      <dl className="grid grid-cols-[1fr_auto] grid-rows-2">
+        <dt className="c-blue-950 font-semibold text-xl">Arcade (Monthly)</dt>
+        <dd className="col-start-2 row-span-full self-center font-bold c-blue-950 text-lg">
+          $90/yr
+        </dd>
+        <dd>
+          <button type="button" className="underline">
+            Change <SROnly> your plan</SROnly>
+          </button>
+        </dd>
       </dl>
     </SummaryHolder>
   );
--- src/features/steps/summary.tsx/summary-holder.tsx
+++ src/features/steps/summary.tsx/summary-holder.tsx
@@ -1,12 +1,25 @@
+import { joinClasses } from "@/shared/libs";
 import React from "react";
 
-export const SummaryHolder = ({ children }: { children: React.ReactNode }) => {
+export const SummaryHolder = ({
+  children,
+  custom: custom = false,
+}: {
+  children: React.ReactNode;
+  custom?: boolean;
+}) => {
   return (
-    <div className="background p-4 not-first:pt-0 first:rounded-t-2xl last:rounded-b-2xl c-grey-500 last:[&>div.mt-4]:hidden">
+    <div
+      className={joinClasses([
+        "background p-4 not-first:pt-0 first:rounded-t-2xl last:rounded-b-2xl c-grey-500 last:[&>div.mt-4]:hidden",
+      ])}
+    >
       {children}
-      <button type="button" className="underline">
-        Change
-      </button>
+      {!custom && (
+        <button type="button" className="underline">
+          Change
+        </button>
+      )}
       <div className="mt-4 border-b b-grey-500" />
     </div>
   );
