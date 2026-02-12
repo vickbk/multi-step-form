823b9e43e46397f02a8baf25999e5de5189279b8
vickbk <vickbake8@gmail.com>
2026-02-12 12:53:55 +0200

    feat: implement AddsOn component with add-on options and styling; add CSS for add-on layout and interactions

--- src/features/steps/adds-on/components/adds-on.tsx
+++ src/features/steps/adds-on/components/adds-on.tsx
@@ -1,3 +1,54 @@
+import { default as Checkmark } from "@/assets/images/icon-checkmark.svg";
+import { StepSection } from "../../components/step-section";
+import "../styles/add-ons.css";
+
 export const AddsOn = ({ show = false }: { show: boolean }) => {
-  return <div>AddsOn</div>;
+  const options = [
+    {
+      name: "online-service",
+      label: "Online service",
+      description: "Access to multiplayer games",
+      price: 10,
+    },
+    {
+      label: "Larger storage",
+      name: "larger-storage",
+      description: "Extra 1TB of cloud save",
+      price: 20,
+    },
+    {
+      label: "Customizable profile",
+      name: "customizable-profile",
+      description: "Custom theme on your profile",
+      price: 20,
+    },
+  ];
+  return (
+    <StepSection
+      show={show}
+      header={{
+        title: "Pick Add-ons",
+        description: "Add-ons help enhance your gaming experience.",
+      }}
+    >
+      {options.map(({ label, name, description, price }) => (
+        <label key={name} className="add-on">
+          <input
+            type="checkbox"
+            className="add-on__input"
+            name="add-on"
+            value={name}
+          />
+          <span className="add-on__icon ">
+            <img src={Checkmark} className="w-8" alt="" />
+          </span>
+          <span className="grid c-grey-500">
+            <span className="font-semibold c-blue-950 text-xl">{label}</span>
+            <span>{description}</span>
+          </span>
+          <span className="c-purple-600 font-medium">+{price}/yr</span>
+        </label>
+      ))}
+    </StepSection>
+  );
 };
