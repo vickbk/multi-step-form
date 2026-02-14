4e777b69f1a52c5c59d8b82f879d4f9db7c6619e
vickbk <vickbake8@gmail.com>
2026-02-14 23:07:05 +0200

    refactor: enhance tests for multi-step form with additional form element checks and navigation logic

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 import { checkHeadingOrder } from "./shared/heading-manager/library/check-heading-order";
 import { drawRegion } from "./shared/heading-manager/library/region-drawer";
@@ -14,4 +15,23 @@ describe("MultiStep form Tests", () => {
     const { container } = render(<App />);
     expect(checkHeadingOrder(drawRegion(container))).toBeTruthy();
   });
+
+  test("should have a form element", async () => {
+    render(<App />);
+    const form = await screen.findByRole("form");
+    expect(form).toBeInTheDocument();
+  });
+
+  test.each([2, 3, 4])(
+    "should not navigate to step %i when the corresponding step button is clicked if no data is processed",
+    async (step) => {
+      render(<App />);
+      await screen.findByText(/personal info/i);
+      const stepButton = await screen.findByRole("button", {
+        name: new RegExp(step.toString()),
+      });
+      await userEvent.click(stepButton);
+      await screen.findByText(/personal info/i);
+    },
+  );
 });
