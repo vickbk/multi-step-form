ddcf21aa0f740934d4822185ece96f2cdc2147cd
vickbk <vickbake8@gmail.com>
2026-02-16 15:11:28 +0200

    Refactor plan selection in tests: replace regex with constants for billing and plan options in multi-step form update tests

--- tests/multisteps-update.spec.ts
+++ tests/multisteps-update.spec.ts
@@ -1,18 +1,22 @@
 import { test } from "@playwright/test";
 import {
   ADVANCED_PLAN_YEARLY,
+  ADVANCED_SELECTOR,
   ARCADE_PLAN_MONTHLY,
+  ARCADE_SELECTOR,
   CUSTOMIZABLE_PROFILE,
   FINISHING_UP_HEADING,
   JOHN_DOE,
   JOHN_DOE_EMAIL,
   JOHN_DOE_PHONE,
   LARGER_STORAGE,
+  MONTHLY_SELECTOR,
   ONLINE_SERVICE,
   shouldSee,
   updateAddOns,
   updatePersonalInfo,
   updatePlan,
+  YEARLY_SELECTOR,
 } from "./stories";
 
 test.describe("Multi-step form - update", () => {
@@ -35,10 +39,16 @@ test.describe("Multi-step form - update", () => {
   test("should update plan selection and reflect the change on summary page", async ({
     page,
   }) => {
-    await updatePlan(page, { billing: /monthly/i, plan: /arcade/i });
+    await updatePlan(page, {
+      billing: MONTHLY_SELECTOR,
+      plan: ARCADE_SELECTOR,
+    });
     await shouldSee(page, [FINISHING_UP_HEADING, ARCADE_PLAN_MONTHLY]);
 
-    await updatePlan(page, { billing: /yearly/i, plan: /advanced/i });
+    await updatePlan(page, {
+      billing: YEARLY_SELECTOR,
+      plan: ADVANCED_SELECTOR,
+    });
     await shouldSee(page, [FINISHING_UP_HEADING, ADVANCED_PLAN_YEARLY]);
   });
 
