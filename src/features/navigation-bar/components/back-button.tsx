bc8d96f5daa269191394a145941559da048b8669
vickbk <vickbake8@gmail.com>
2026-02-17 15:07:17 +0200

    feat: add useKeyboardBack hook for keyboard navigation support

--- src/features/navigation-bar/components/back-button.tsx
+++ src/features/navigation-bar/components/back-button.tsx
@@ -1,10 +1,13 @@
 import { saveAndGoTo } from "@/features/sidebar/scripts";
 import { SROnly } from "@/shared/components/SROnly";
 import { joinClasses } from "@/shared/libs";
+import { useKeyboardBack } from "../hooks/use-keyboard-back";
 
 export const BackButton = ({ isFirstStep }: { isFirstStep: boolean }) => {
+  const backButtonRef = useKeyboardBack();
   return (
     <button
+      ref={backButtonRef}
       className={joinClasses([
         "c-grey-500 font-semibold active-button outline-none! active-c-blue-950",
         isFirstStep && "hidden",
