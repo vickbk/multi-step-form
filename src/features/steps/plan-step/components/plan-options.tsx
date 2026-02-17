17a27808c878bd686d731b9d562eb1c2039a4963
vickbk <vickbake8@gmail.com>
2026-02-17 18:51:17 +0200

    feat: set default plan to "arcade" in PlanOptions component and remove unused plan error tests

--- src/features/steps/plan-step/components/plan-options.tsx
+++ src/features/steps/plan-step/components/plan-options.tsx
@@ -2,7 +2,7 @@ import { SROnly } from "@/shared/components/SROnly";
 import { PLANS_INPUTS } from "../scripts";
 import type { PlanType } from "../types";
 
-export const PlanOptions = ({ plan, billing }: PlanType) => {
+export const PlanOptions = ({ plan = "arcade", billing }: PlanType) => {
   return (
     <fieldset className="plan">
       <legend className="sr-only">Plan Options</legend>
