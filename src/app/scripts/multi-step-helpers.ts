d94c743a43f4129c22d4cd3035fce0c185e459cc
vickbk <vickbake8@gmail.com>
2026-02-17 12:34:52 +0200

    Fix: ensure correct return type for multistepsSubmitHandler on last step

--- src/app/scripts/multi-step-helpers.ts
+++ src/app/scripts/multi-step-helpers.ts
@@ -10,13 +10,13 @@ export async function multistepsSubmitHandler({
   data,
   isLastStep,
   step,
-}: MultiStepHandlerParams<MultiStepData>) {
+}: MultiStepHandlerParams<MultiStepData>): Promise<MultiStepData> {
   const formData = Object.fromEntries(data);
   const results: MultiStepData = { ...previous, ...formData };
   if ("go-to" in results) delete results["go-to"];
   if (isLastStep) {
     await submitMultiStepSample(results);
-    return {};
+    return {} as MultiStepData;
   }
   if (step === 2) {
     return { ...results, "add-ons": data.getAll("add-ons") as string[] };
